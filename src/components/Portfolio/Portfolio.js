1a1f47a9b157e2d988edb6e5b08c17c86366c852
Harsh Khare <harshkhare3@gmail.com>
2021-03-22 02:07:27 +0530

    rectifying warnings

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '../Card/Card';
 import Fade from 'react-reveal/Fade';
 import Button from '@material-ui/core/Button';
