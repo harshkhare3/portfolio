55a98f842c90b3ca4b335bc8a8a3804682722d8d
Harsh Khare <harshkhare3@gmail.com>
2021-03-24 23:21:22 +0530

    Added Card Shadow

--- src/components/Portfolio/Data.js
+++ src/components/Portfolio/Data.js
@@ -2,6 +2,7 @@ import amazonClone from '../../images/amazon.jpg';
 import Alan from '../../images/alan.jpg';
 import Zoom from '../../images/zoom.png';
 import Roster from '../../images/roster.jpg';
+import Covid from '../../images/covid.jpg';
 
 
 export const WebApp = [
@@ -18,34 +19,49 @@ export const WebApp = [
         <li>Orders page to show previously placed orders.</li>
         <li>Fully functional stripe payment method integrated (currently running on test keys).</li>
       </ul>`,
-      tech:`<p>Tech Used - ReactJs, React Context API & Firebase Cloud Functions.</p>`
+      tech:`<p>Tech Used - ReactJs, React Context API & Firebase.</p>`
     },
     {
       name: "Roster Project",
       imageUrl: Roster,
       id: 1,
-      data: '',
+      data: `It is a task/chore roster system for shared spaces that allows managers to set tasks, notifies users of their tasks, and allows users to mark tasks as complete.
+      <br/><br/>
+      Features:-<br/><br/>
+      <ul>
+        <li>Authentication for managers and employees.</li>
+        <li>Employees can view task, set email noti & mark a task as complete.</li>
+        <li>Managers can easily set a roster acording to employees availability.</li>
+      </ul>
+      `,
       tech: `<p>Tech Used - JavaScript, VueJs, NodeJs & MySQL.</p>`
     },
     {
-      name: "Alan AI ",
-      imageUrl: Alan,
+      name: "COVID-19 Tracker App",
+      imageUrl: Covid,
       id: 1,
-      data: '',
-      tech: `<p>Tech Used - Alan AI Voice Assistant & ReactJs.</p>`
+      data: `It is a COVID-19 Tracker App where you can easily track the number of people infected and recovered by COVID-19. <br/>
+      All the data on this website comes from disease.sh API. <br/><br/>
+      Features:-<br/><br/>
+      <ul>
+        <li>Realtime data coming from disease.sh</li>
+        <li>All the data is shown graphically, and on map according to the country.</li>
+      </ul>
+      `,
+      tech: `<p>Tech Used - ReactJs</p>`
     },
     {
       name: "Zoom Flutter Clone",
       imageUrl: Zoom,
       id: 2,
-      data: '',
+      data: ``,
       tech: `<p>Tech Used - Flutter & Firebase.</p>`
     },
     {
       name: "Firebase CHat App",
       imageUrl: Zoom,
       id: 2,
-      data: '',
+      data: ``,
       tech: `<p>Tech Used - Flutter & Jitsi Meet.</p>`
     }
   ]
