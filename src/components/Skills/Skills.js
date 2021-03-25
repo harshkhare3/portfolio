801be3ea8925341ed0a4e23229337a659f4a85f6
Harsh Khare <harshkhare3@gmail.com>
2021-03-25 19:23:11 +0530

    refactored

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,17 +3,13 @@ import './Skills.scss';
 
 const Skills = ({skill, progress, width}) => {
   return (
-    <div className="skills">
-      <div className="skills__container">
-        <h5 className="skills__title">{skill}</h5>
-        <div className="sills__bar">
-          <p className="skills__text">{progress}</p>
-          <div className="skills__progress">
-            <div className="skills__progressBar">
-              <div className="skills__innerProgress" style={{ width: width}}>
-                
-              </div>
-            </div>
+    <div className="skills__container">
+      <h5 className="skills__title">{skill}</h5>
+      <div className="sills__bar">
+        <p className="skills__text">{progress}</p>
+        <div className="skills__progress">
+          <div className="skills__progressBar">
+            <div className="skills__innerProgress" style={{ width: width}}>    </div>
           </div>
         </div>
       </div>
