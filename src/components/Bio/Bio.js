10d3e332ffc1025f1221458639ce53d72cb955a8
Harsh Khare <harshkhare3@gmail.com>
2021-03-25 18:39:50 +0530

    Style Changed

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -82,20 +82,23 @@ const Bio = () => {
           </div>
         </div>
       </Fade>
-      <div className="bio__skills">
-        <h1>MY SKILLS</h1>
-        <div className="bio__skillsContainer">
-          <Skills skill={'Javascript'} progress={'60%'} width={'60%'} />
-          <Skills skill={'Typescript'} progress={'30%'} width={'30%'} />
-          <Skills skill={'React Js'} progress={'70%'} width={'70%'} />
-          <Skills skill={'Node Js'} progress={'80%'} width={'80%'} />
-          <Skills skill={'Python'} progress={'90%'} width={'90%'} />
-          <Skills skill={'Lua'} progress={'40%'} width={'40%'} />
-          <Skills skill={'Java'} progress={'80%'} width={'80%'} />
-          <Skills skill={'Web Design'} progress={'25%'} width={'25%'} />
+      <Fade duration="2000">
+        <div className="bio__skills">
+          <h1>MY SKILLS</h1>
+          <div className="bio__skillsContainer">
+            <Skills skill={'Javascript'} progress={'60%'} width={'60%'} />
+            <Skills skill={'Typescript'} progress={'30%'} width={'30%'} />
+            <Skills skill={'React Js'} progress={'70%'} width={'70%'} />
+            <Skills skill={'Node Js'} progress={'80%'} width={'80%'} />
+            <Skills skill={'Python'} progress={'90%'} width={'90%'} />
+            <Skills skill={'Lua'} progress={'40%'} width={'40%'} />
+            <Skills skill={'Java'} progress={'80%'} width={'80%'} />
+            <Skills skill={'Web Design'} progress={'25%'} width={'25%'} />
+          </div>
         </div>
-      </div>
+      </Fade>
     </div>
+    
   )
 }
 
