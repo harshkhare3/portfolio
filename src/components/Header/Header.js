a945c3d2c6e08479aa5ea97a9c248f5840f79e85
Harsh Khare <harshkhare3@gmail.com>
2021-03-26 18:42:02 +0530

    fix for ipad

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-scroll';
 import LinkScroll from '../LinkScroll/LinkScroll';
 import './Header.scss';
+import Headroom from 'react-headroom';
 
 function Header(props) {
   const [scrollNav, setScrollNav] = useState(false);
@@ -21,35 +22,37 @@ function Header(props) {
   }, []);
 
   return (
-    <div className={scrollNav === true ? "header__nav" : "header__nav"} >
-      <div className="header__container">
-        <Link
-          smooth={true} 
-          duration={500} 
-          offset={0} 
-          to="home" 
-          className="header__logo"
-        >
-          Harsh Khare
-        </Link>
-        
-        <div onClick={props.toggle} className="header__mobileIcon">
-          <FaBars />
-        </div>
+    <Headroom disableInlineStyles="true">
+      <div className={scrollNav === true ? "header__nav" : "header__nav"} >
+        <div className="header__container">
+          <Link
+            smooth={true} 
+            duration={500} 
+            offset={0} 
+            to="home" 
+            className="header__logo"
+          >
+            Harsh Khare
+          </Link>
           
-        <ul className="header__navMenu">
-          <li className="header__navItem">
-            <LinkScroll to="about" name="About" />
-          </li>
-          <li className="header__navItem">
-            <LinkScroll to="portfolio" name="Portfolio" />
-          </li>
-          <li className="header__navItem">
-            <LinkScroll to="contact" name="Contact Me" />
-          </li>
-        </ul>
+          <div onClick={props.toggle} className="header__mobileIcon">
+            <FaBars />
+          </div>
+            
+          <ul className="header__navMenu">
+            <li className="header__navItem">
+              <LinkScroll to="about" name="About" />
+            </li>
+            <li className="header__navItem">
+              <LinkScroll to="portfolio" name="Portfolio" />
+            </li>
+            <li className="header__navItem">
+              <LinkScroll to="contact" name="Contact Me" />
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </Headroom>
   )
 }
 
